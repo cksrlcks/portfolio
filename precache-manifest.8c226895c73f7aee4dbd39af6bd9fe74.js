f6323d84a34d9ea85d6c31b6035eb8fa68242b90
chanki <chanki.kim89@gmail.com>
2020-02-09 01:34:35 +0900

    Updates

---
+++ precache-manifest.8c226895c73f7aee4dbd39af6bd9fe74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2248eab2548bcde9712f3044994707c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9db16a27e1f546163dd2",
+    "url": "/static/css/main.bf1b6bde.chunk.css"
+  },
+  {
+    "revision": "8e8812e62312e1a797b4",
+    "url": "/static/js/2.6763ec1d.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/static/js/2.6763ec1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9db16a27e1f546163dd2",
+    "url": "/static/js/main.5592bd35.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "a33b19c06f096cc550d4cfcc930dc96a",
+    "url": "/static/media/logo.a33b19c0.svg"
+  }
+]);
