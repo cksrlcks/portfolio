cbde8ee747b764c12f850779dcb455dd597e131b
chanki.kim <chanki.kim89@gmail.com>
2025-03-23 12:53:15 +0900

    init: project setting

---
+++ README.md
@@ -0,0 +1,14 @@
+![Group 22](https://github.com/user-attachments/assets/64c8f2c0-1950-41a0-a880-ce4e720598ed)
+# Portfolio
+프론트앤드 개발자 김찬기의 포트폴리오 입니다.
+<h4>
+  <a href="#">Portfolio Site</a> |
+  <a href="#">Resume PDF</a>
+</h4>   
+
+---
+
+## Introduce
+
+(소개작성)
+
---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import { Inter } from 'next/font/google';
+import type { Metadata } from 'next';
+import './globals.css';
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  display: 'swap',
+});
+
+export const metadata: Metadata = {
+  title: '김찬기 | 포트폴리오',
+  description: '프론트앤드 개발자 김찬기의 포트폴리오입니다.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='ko'>
+      <head>
+        <link
+          rel='stylesheet'
+          as='style'
+          crossOrigin='anonymous'
+          href='https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css'
+        />
+      </head>
+      <body className={`${inter.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
