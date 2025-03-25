5acc034c3822aee9e0e4742ca36a8a5a6f3bb07a
chanki.kim <chanki.kim89@gmail.com>
2025-03-25 11:48:45 +0900

    feat: 블로그 링크 추가

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { RiGithubFill } from '@remixicon/react';
+import TistoryIcon from '@/assets/img/icon-tistory.svg';
 
 export default function Profile() {
   return (
@@ -14,7 +16,7 @@ export default function Profile() {
         당연한 일도 다른 시각으로 바라보며 새로운 가능성을 찾습니다. 경험을
         소중히 여기며, 늘 궁금해하고 배워가며 성장합니다.
       </p>
-      <div className='font-inter flex gap-4 text-sm lg:mb-14'>
+      <div className='font-inter flex flex-wrap gap-x-4.5 gap-y-2 text-sm lg:mb-14'>
         <a
           href='https://github.com/cksrlcks'
           target='_blank'
@@ -23,6 +25,15 @@ export default function Profile() {
         >
           <RiGithubFill size={16} />
           GITHUB
+        </a>
+        <a
+          href='https://heavy-bear.tistory.com/'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='flex items-center gap-1.5 font-semibold text-white hover:underline'
+        >
+          <Image src={TistoryIcon} alt='tistory' className='w-4' />
+          BLOG
         </a>
         <a
           href='mailto:chanki.kim89@gmail.com'
