11cb8098109f962c0db2f0b4cbbbadb50d92ad18
chanki.kim <chanki.kim89@gmail.com>
2025-03-28 21:00:10 +0900

    feat: opengraph 작업

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,12 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: '김찬기 | 포트폴리오',
   description: '프론트앤드 개발자 김찬기의 포트폴리오입니다.',
+  openGraph: {
+    type: 'website',
+    siteName: '김찬기 | 포트폴리오',
+    title: '김찬기 | 포트폴리오',
+    description: '프론트앤드 개발자 김찬기의 포트폴리오입니다.',
+  },
 };
 
 export default function RootLayout({
