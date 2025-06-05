8df0af341d233b67d0c85211592e06463243dc1a
chanki.kim <chanki.kim89@gmail.com>
2025-06-05 13:31:53 +0900

    feat: add redirects for old domain

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   transpilePackages: ['next-mdx-remote'],
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'heavybear-portfolio.vercel.app',
+          },
+        ],
+        destination: 'https://heavybear.net',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
